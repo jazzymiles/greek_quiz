2c45997d1fe8ccaf0d87347a882e6ab03e5a121a
Alexanader "Jazzy Miles" Redinger <jazzman.miles@gmail.com>
2025-08-13 01:49:51 +0300

    - 7

--- android/app/src/main/kotlin/com/example/greek_quiz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/greek_quiz/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.greek_quiz
 
 import com.ryanheise.audioservice.AudioServiceActivity
 
-// Достаточно просто унаследоваться — без configureFlutterEngine и setActivity.
 class MainActivity : AudioServiceActivity()
